6be72adc803030f76ee4047f3f97689bcbbec9d4
lanyuping <szrwlyp@163.com>
2024-01-26 09:38:01 +0800

    first commit

---
+++ src/components/CustomNavBar/CustomNavBar.vue
@@ -0,0 +1,114 @@
+<script setup lang="ts">
+import type { Long } from "typeorm";
+import { ref, onMounted, reactive, useSlots } from "vue";
+
+const slots = useSlots();
+console.log(slots);
+
+type modeType = "includeMenu" | "onIncludeMenu";
+interface Props {
+  backgroundColor?: string;
+  customNavBarStyle?: string;
+  mode: modeType;
+}
+const props = withDefaults(defineProps<Props>(), {
+  customNavBarStyle: "",
+  mode: "includeMenu",
+});
+
+// 状态栏高度
+const statusHeight = ref(0);
+
+// 导航栏内容高度
+const navContentHeight = ref(0);
+
+const setNavBar = (systemInfo: any, rect: any) => {
+  let { statusBarHeight } = systemInfo;
+
+  let { top, height } = rect;
+
+  // 设置状态栏高度
+  statusHeight.value = statusBarHeight;
+
+  // 设置导航栏内容高度
+  navContentHeight.value = (top - statusBarHeight) * 2 + height;
+};
+
+const navBarStyle = ref("");
+const setNavBarArrange = (systemInfo: any, rect: any) => {
+  const { statusBarHeight, system, screenWidth, windowWidth } = systemInfo;
+  console.log(systemInfo);
+  console.log(rect);
+
+  if (props.mode === "includeMenu") {
+    navBarStyle.value = `width:100%;`;
+  } else {
+    navBarStyle.value = `width:${rect.left}px;${props.customNavBarStyle}`;
+  }
+};
+onMounted(() => {
+  // 获取系统信息
+  const systemInfo = uni.getSystemInfoSync();
+
+  // 获取胶囊信息
+  const rect = uni.getMenuButtonBoundingClientRect();
+
+  setNavBar(systemInfo, rect);
+  // 设置导航栏内容排列
+  setNavBarArrange(systemInfo, rect);
+});
+</script>
+
+<template>
+  <view :style="{ height: `${statusHeight + navContentHeight}px` }"></view>
+  <view
+    class="custom-nav-bar"
+    :style="{
+      'background-color': props.backgroundColor,
+    }"
+  >
+    <!-- 状态栏 -->
+    <view :style="{ height: `${statusHeight}px` }"></view>
+
+    <view
+      class="nav-bar"
+      :style="{ height: `${navContentHeight}px`, ...navBarStyle }"
+    >
+      <!-- 左侧内容，例如返回按钮 -->
+      <view v-if="slots.center || slots.left" class="flex-items"
+        ><slot name="left"></slot
+      ></view>
+      <!-- 中间内容，例如标题 -->
+      <view v-if="slots.center" class="flex-items flex-center"
+        ><slot name="center"></slot
+      ></view>
+      <!-- 右侧内容，例如更多按钮 -->
+      <view v-if="slots.center || slots.right" class="flex-items"
+        ><slot name="right"></slot
+      ></view>
+    </view>
+  </view>
+</template>
+
+<style lang="scss" scoped>
+.custom-nav-bar {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 999;
+  .nav-bar {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    .flex-items {
+      flex: 1;
+    }
+    .flex-center {
+      display: flex;
+      justify-content: center;
+    }
+  }
+}
+</style>
---
+++ src/pages/index/index.vue
@@ -0,0 +1,58 @@
+<template>
+  <view class="content">
+    <CustomNavBar>
+      <template #left>
+        <view
+          style="
+            margin-left: 16px;
+            font-size: 18px;
+            color: #222222;
+            font-weight: 500;
+          "
+          >首页</view
+        >
+      </template>
+      <!-- <template #center>
+        <view>智慧楼宇</view>
+      </template>
+      <template #right>
+        <view>智慧楼宇</view>
+      </template> -->
+    </CustomNavBar>
+    <view class="bg-color"></view>
+    <view class="top-right"></view>
+    <view style="display: flex; justify-content: center">智慧楼宇</view>
+  </view>
+</template>
+
+<script setup lang="ts">
+import CustomNavBar from "@/components/CustomNavBar/CustomNavBar.vue";
+import { ref } from "vue";
+const title = ref("Hello");
+</script>
+
+<style lang="scss" scoped>
+.content {
+  position: relative;
+  min-height: 100vh;
+  background: #f5f9fa;
+  .bg-color {
+    /* background: linear-gradient(90deg, #b0e8de 0%, #b0e8de 100%); */
+    background: linear-gradient(#b0e8de 0%, #f5f9fa 100%);
+    width: 100%;
+    height: 300px;
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+  .top-right {
+    width: 240px;
+    height: 240px;
+    background: #85e0ff;
+    position: absolute;
+    top: -75px;
+    left: 212px;
+    filter: blur(130px);
+  }
+}
+</style>
